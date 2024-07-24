bdd3a3a088031e54a27a010286f5ae2938c9762e
ryanhan <ryanjhan10@gmail.com>
2024-07-24 01:21:09 -0400

    first

---
+++ 4-names.js
@@ -0,0 +1,8 @@
+//local
+const secret = `SUPER SCRET!!!`
+
+//share
+const john = `john`
+const peter = `peter`
+
+module.exports = {john, peter}
---
+++ app.js
@@ -0,0 +1,6 @@
+const names = require('./4-names')
+const sayHi = require('./5-utils')
+
+sayHi('susan')
+sayHi(names.john)
+sayHi(names.peter)
